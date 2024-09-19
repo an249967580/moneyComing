2d5fabd7cfd01fe703dab51ec3d5a50523ea4dd2
xlt <@qq.com>
2024-09-19 21:51:11 +0800

    1

--- build/moneyComing/src/settings.c2abd.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/loading.fire",orientation:"",jsList:[],bundleVers:{resources:"b9fb9",internal:"d17e2",main:"9a045"}};
